bc654a8822fa3183e49acb4bdd64e9e80054e618
Grégoire Aubertin <greg_abrn@icloud.com>
2025-05-26 15:09:24 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <section>
+        <h1>Lorem</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus ea,
+          architecto nobis enim ipsam eum tenetur provident sapiente sequi?
+          Recusandae placeat aliquam eius nobis sequi nam molestias maiores
+          voluptate fugit? Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Totam temporibus explicabo tempora similique perspiciatis
+          exercitationem quasi nesciunt eveniet eius inventore laudantium
+          mollitia repudiandae, aspernatur quia debitis natus voluptatem dicta
+          distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Aperiam voluptas blanditiis saepe tempora. Consequuntur adipisci
+          doloribus asperiores vel ipsum voluptatum harum quae! Autem, doloribus
+          assumenda! Quo corrupti in omnis harum!
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default App;
